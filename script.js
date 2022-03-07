1ab7fcc9a635bdea0dc568bdc63d560b4a4543b7
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-07 13:04:50 -0600

    HTML boilerplate, also made basic versions of enemy and player object

---
+++ script.js
@@ -0,0 +1,32 @@
+// Global Variables
+
+// Obects + constructors
+
+const player = {
+  playerName: 'name',
+  playerHp: 100,
+  playerMp: 100,
+  playerAttacks: [],
+  playerItems: [],
+  guard() {
+    console.log('En guarde!')
+  },
+  attack() {
+    console.log('Take this!')
+  }
+}
+
+const firstEnemy = {
+  enemyName: 'first enemy',
+  enemyHp: 100,
+  enemyMp: 100,
+  enemyAttacks: [],
+  enemyPhrases: [],
+  attack() {
+    console.log('Take this!')
+  }
+}
+
+// Functions
+
+// Event Listeners
