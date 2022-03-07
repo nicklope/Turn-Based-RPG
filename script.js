8ad327b2b9463755ce4f2dc44250eaaf80ce1752
Nick Lopez <nicklopez5454@yahoo.com>
2022-03-07 13:20:35 -0600

    Added very basic battle system

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 // Global Variables
 
+// Functions
+
+const enemyTurn = () => {
+  console.log(`${firstEnemy.enemyName} came out swinging!`)
+  player.playerHp -= firstEnemy.enemyAttacks
+  console.log(
+    `${player.playerName} took ${firstEnemy.enemyAttacks} points of damage!`
+  )
+}
+
 // Obects + constructors
 
 const player = {
   playerName: 'name',
   playerHp: 100,
   playerMp: 100,
-  playerAttacks: [],
+  playerAttacks: 10,
   playerItems: [],
   guard() {
     console.log('En guarde!')
   },
-  attack() {
-    console.log('Take this!')
+  attack(enemy) {
+    console.log(`${this.playerName} goes for the attack!`)
+    enemy.enemyHp -= this.playerAttacks
+    console.log(
+      `${enemy.enemyName} took ${this.playerAttacks} points of damage!`
+    )
+    enemyTurn()
   }
 }
 
@@ -20,13 +35,13 @@ const firstEnemy = {
   enemyName: 'first enemy',
   enemyHp: 100,
   enemyMp: 100,
-  enemyAttacks: [],
+  enemyAttacks: 10,
   enemyPhrases: [],
   attack() {
     console.log('Take this!')
   }
 }
 
-// Functions
-
 // Event Listeners
+
+console.log(player.attack(firstEnemy))
